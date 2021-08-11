1b07d64ebe1a148b0bc155cee83bb47302b05191
tesfaabasmelash <asmelashtesfaab@gmail.com>
2021-08-11 18:46:06 -0500

    the Second Commit

---
+++ src/GitClass/GitClass01.java
@@ -0,0 +1,9 @@
+package GitClass;
+
+public class GitClass01 {
+public static void main(String[] args) {
+	
+	System.out.println("This is the first code");
+}
+	
+}
--- src/GitClass/GitClass03.java
+++ src/GitClass/GitClass03.java
@@ -1,5 +1,10 @@
 package GitClass;
 
 public class GitClass03 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is the first code");
+	}
 
 }
